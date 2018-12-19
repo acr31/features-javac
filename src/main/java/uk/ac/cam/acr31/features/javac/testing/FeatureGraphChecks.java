503fdd653888578ed000047e68bd48c93a59ac96
Andrew Rice <acr31@cam.ac.uk>
2018-12-19 11:22:03 +0000

    added edgeBetween check and fail fast if there is any ambiguity in the query

--- src/main/java/uk/ac/cam/acr31/features/javac/testing/FeatureGraphChecks.java
+++ src/main/java/uk/ac/cam/acr31/features/javac/testing/FeatureGraphChecks.java
@@ -7,36 +7,51 @@ import com.google.common.collect.Iterables;
 import java.util.HashSet;
 import java.util.Set;
 import uk.ac.cam.acr31.features.javac.graph.FeatureGraph;
-import uk.ac.cam.acr31.features.javac.proto.GraphProtos;
+import uk.ac.cam.acr31.features.javac.proto.GraphProtos.FeatureEdge.EdgeType;
+import uk.ac.cam.acr31.features.javac.proto.GraphProtos.FeatureNode;
 
 public class FeatureGraphChecks {
 
-  public static ImmutableList<String> pathTo(FeatureGraph graph, String destination) {
-    return pathTo(
+  private static FeatureNode findNode(FeatureGraph graph, String contents) {
+    return Iterables.getOnlyElement(
+        graph
+            .nodes()
+            .stream()
+            .filter(n -> n.getContents().equals(contents))
+            .collect(toImmutableList()));
+  }
+
+  public static boolean edgeBetween(
+      FeatureGraph graph, String source, String destination, EdgeType edgeType) {
+    FeatureNode sourceNode = findNode(graph, source);
+    FeatureNode destinationNode = findNode(graph, destination);
+    return graph
+        .edges(sourceNode, destinationNode)
+        .stream()
+        .anyMatch(e -> e.getType().equals(edgeType));
+  }
+
+  public static ImmutableList<String> astPathToToken(FeatureGraph graph, String destination) {
+    FeatureNode destinationNode = findNode(graph, destination);
+    return astPathToToken(
         graph,
-        Iterables.getOnlyElement(graph.nodes(GraphProtos.FeatureNode.NodeType.AST_ROOT)),
-        destination,
+        Iterables.getOnlyElement(graph.nodes(FeatureNode.NodeType.AST_ROOT)),
+        destinationNode,
         new HashSet<>());
   }
 
-  private static ImmutableList<String> pathTo(
-      FeatureGraph graph,
-      GraphProtos.FeatureNode featureNode,
-      String destination,
-      Set<Long> visited) {
+  private static ImmutableList<String> astPathToToken(
+      FeatureGraph graph, FeatureNode featureNode, FeatureNode destinationNode, Set<Long> visited) {
     if (visited.contains(featureNode.getId())) {
       return ImmutableList.of();
     }
     visited.add(featureNode.getId());
-    if (featureNode.getContents().equals(destination)) {
+    if (featureNode.equals(destinationNode)) {
       return ImmutableList.of(featureNode.getContents());
     }
-    for (GraphProtos.FeatureNode node :
-        graph.successors(
-            featureNode,
-            GraphProtos.FeatureEdge.EdgeType.AST_CHILD,
-            GraphProtos.FeatureEdge.EdgeType.ASSOCIATED_TOKEN)) {
-      ImmutableList<String> rest = pathTo(graph, node, destination, visited);
+    for (FeatureNode node :
+        graph.successors(featureNode, EdgeType.AST_CHILD, EdgeType.ASSOCIATED_TOKEN)) {
+      ImmutableList<String> rest = astPathToToken(graph, node, destinationNode, visited);
       if (!rest.isEmpty()) {
         return ImmutableList.<String>builder().add(featureNode.getContents()).addAll(rest).build();
       }
@@ -45,11 +60,7 @@ public class FeatureGraphChecks {
   }
 
   public static ImmutableList<String> getNodeContents(
-      FeatureGraph graph, GraphProtos.FeatureNode.NodeType nodeType) {
-    return graph
-        .nodes(nodeType)
-        .stream()
-        .map(GraphProtos.FeatureNode::getContents)
-        .collect(toImmutableList());
+      FeatureGraph graph, FeatureNode.NodeType nodeType) {
+    return graph.nodes(nodeType).stream().map(FeatureNode::getContents).collect(toImmutableList());
   }
 }
